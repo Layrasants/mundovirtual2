faac6e8d14614ede47855077b97a33e044604b0e
Layrasants <131265285+Layrasants@users.noreply.github.com>
2025-02-07 19:16:57 -0300

    Deixar Expo Doctor passando tudo verde #2

--- app/_layout.js
+++ app/_layout.js
@@ -1,11 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Stack } from 'expo-router';
-
 
 <Stack>
     <Stack.Screen name="index" options={{ headerShown: false }} />
     <Stack.Screen name="login" options={{ headerShown: false }} />
     <Stack.Screen name="cadastros" options={{ headerShown: false }} />
 </Stack>
-
-
